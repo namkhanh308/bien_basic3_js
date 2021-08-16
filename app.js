52a5bee431830af1ccc21a954b7f2c3b1a9a985e
Khanh <uefa.euro30@gmail.com>
2021-08-16 21:42:14 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+// bai1
+// let ly = prompt("Nhập điểm môn vật lý")
+// let hoa = prompt("Nhập điểm môn hóa")
+// let sinh = prompt("Nhập điểm môn sinh")
+// a = parseInt(ly)
+// b = parseInt(hoa)
+// c = parseInt(sinh)
+// let tong = a + b + c
+// alert("Tổng điểm 3 môn của bạn là: " + tong)
+// alert("Điểm trung bình của bạn là: "+ tong/3)
+
+// bai2
+// let Celsius = prompt("nhập độ c: ")
+// let c = parseInt(Celsius)
+// let f = (9/5)*c + 32
+// alert(f)
+
+// bai3
+// let pi = 3.14
+// let a = parseInt(prompt("nhập vào bán kính hình tròn"))
+// alert("Diện tích hình tròn là"+ pi*a*a)
+
+// bai4
+// let pi = 3.14
+// let b = parseInt(prompt("Nhập vào bán kính hình tròn: "))
+// alert("Chu vi hình tròn là: " + 2*pi*b)
